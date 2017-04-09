8d6073b41ea0628924c6b9174ae68644b831852f
Kendrick Arnett <kmarnett@gmail.com>
2017-04-09 16:43:12 -0500

    feat(Build): Update copy task to copy media directories.

--- gulp-tasks/copy.js
+++ gulp-tasks/copy.js
@@ -3,6 +3,12 @@
 
   module.exports = function (gulp, plugins, config) {
     return function () {
+      var parent = '';
+
+      gulp.src([config.srcPath + '/**/media/**/*'], { base: config.srcPath + '/decks' })
+        .pipe(gulp.dest(config.buildPath))
+      ;
+
       gulp.src([config.srcPath + '/lib/**/*'])
         .pipe(plugins.plumber({ errorHandler: plugins.notify.onError('Error: <%= error.message %>') }))
         .pipe(gulp.dest(config.buildPath + '/lib'))
