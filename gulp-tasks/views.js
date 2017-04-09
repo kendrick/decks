694ec48c60591fd06e98c151f08d9669c6a8221f
Kendrick Arnett <kmarnett@gmail.com>
2017-04-09 16:42:45 -0500

    fix(Build): Exclude media directory from slide accretion.

--- gulp-tasks/views.js
+++ gulp-tasks/views.js
@@ -5,15 +5,17 @@
     return function () {
       var fs = require('fs');
       var path = require('path');
-      var data = require('gulp-data');
       var pug = require('pug');
 
       var decksPath = config.srcPath + '/decks';
 
-      return gulp.src([decksPath + '/**/*.pug', '!' + decksPath + '/**/_*.pug'])
+      return gulp.src([
+          decksPath + '/**/*.pug',
+          '!' + decksPath + '/**/_*.pug',
+        ])
         // .pipe(plugins.changed(config.buildPath, { extension: '.html' }))
         .pipe(plugins.plumber({ errorHandler: plugins.notify.onError('Error: <%= error.message %>') }))
-        .pipe(data(function (file) {
+        .pipe(plugins.data(function (file) {
           var fileDir = path.dirname(file.path);
           var relPath = path.relative(decksPath, fileDir);
           var slides;
@@ -27,14 +29,14 @@
             absPath: fileDir
           }
         }))
-        .pipe(data(function (file) {
+        .pipe(plugins.data(function (file) {
           var absPath = file.data.absPath;
           var html = '';
 
           if (file.data.slides) {
             var len = file.data.slides.length;
             for (var i = 0; i < len; i++) {
-              if (file.data.slides[i] !== 'index.pug') {
+              if (file.data.slides[i] !== 'index.pug' && file.data.slides[i] !== 'media') {
                 html += pug.renderFile(absPath + '/' + file.data.slides[i], { pretty: true });
               }
             }
